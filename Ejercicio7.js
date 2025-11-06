f51a8ff62a53a0c75361f7dd45591dfd23938781
Luis_villamizar <Luisca123c@gmail.com>
2025-11-06 17:36:40 -0500

    Septimo ejercicio actualizado

---
+++ Ejercicio7.js
@@ -0,0 +1,11 @@
+let suma = 0;
+for (let i = 1; i <= 100; i++) 
+    {
+        if (i >90 && i % 2 == 0)
+        {
+            break;
+        }
+        else{
+            suma += i;
+        }
+    }
